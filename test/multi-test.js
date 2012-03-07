117d024573f926747ba8d337c7f9ed0f2201adc4
fent <933490+fent@users.noreply.github.com>
2012-03-07 02:08:15 -0700

    [fix] correctly remove wraper listener

--- test/multi-test.js
+++ test/multi-test.js
@@ -7,11 +7,19 @@ describe('Listen for an event more than once', function() {
   var yoshi = new EventYoshi();
   yoshi.add(new EventEmitter());
   yoshi.add(new EventEmitter());
+  function a() {}
+  function b() {}
+  yoshi.on('foo', a);
+  yoshi.on('foo', b);
 
   it('Should emit event only once when emitted', function(done) {
-    yoshi.on('foo', function() {});
-    yoshi.on('foo', function() {});
     yoshi.on('foo', done);
     yoshi.emit('foo');
+  });
+
+  describe('remove a listener', function() {
+    it('Does not throw', function() {
+      yoshi.removeListener('foo', a);
+    });
   });
 });
